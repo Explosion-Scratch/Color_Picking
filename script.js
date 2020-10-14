53ac1555e4e04b8362d47f16788f3e369ae1512f
--Explosion-- <explosion.scratch@gmail.com>
2020-10-14 20:24:26 +0000

    Yay! It works!

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function colors() {
 function setcolor(element) {
 	var thing = '#color-' + element
 	var color = 'rgba(' + random() + ',' + random() + ',' + random() + ')';
-	$(thing).css('background-color', 'red')
+	$(thing).css('background-color', color)
 }
 function random() {
 	return Math.floor((Math.random() * 255) + 1);
