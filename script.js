688c3259dfb79a98425738a425a373e0164b61cb
--Explosion-- <explosion.scratch@gmail.com>
2020-10-14 22:37:42 +0000

    New game banner stuffz

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var no = random(6);
 var correct = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
 colors();
 $('#lookfor').html(correct);
-$('#new-game').hide();
+//$('#new-game').hide();
 function newgame() {
 	no = random(6);
   correct = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
 	colors();
 	$('#lookfor').html(correct);
 	$('.color').css('opacity', '1');
-	$('#new-game').slideDown();
+	//$('#new-game').slideDown();
 	//setTimeout(() => {  $('#new-game').slideUp() }, 2000);
 
 }
